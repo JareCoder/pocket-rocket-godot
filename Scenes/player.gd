4d523f9c9725c9faf9a1c93321463dd05d0bee95
Jesse Lappalainen <jesse.m.lappalainen@gmail.com>
2026-06-10 20:33:30 +0300

    Feat: Implement movement

---
+++ Scenes/player.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@export var speed: int = 500
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position = Vector2(100, 100)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var dir = Input.get_vector("left", "right", "up", "down")
+	position += dir * speed * delta
