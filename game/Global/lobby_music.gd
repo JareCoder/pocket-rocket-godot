abdcfe781db00c0407d4712a2439d3d726c91c99
Jesse Lappalainen <jesse.m.lappalainen@gmail.com>
2026-06-21 00:42:47 +0300

    Fix: Fix main menu music looping

--- game/Global/lobby_music.gd
+++ game/Global/lobby_music.gd
@@ -4,6 +4,7 @@ func _ready() -> void:
 	bus = "LobbyMusic"
 	stream = load("res://Audio/Music/Calima - Usual Rigmarole.mp3")
 	volume_db = -25.0
+	finished.connect(_on_finished)
 	play()
 
 func _on_finished() -> void:
