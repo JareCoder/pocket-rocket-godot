01feab3bb4bb364d6a5440b28cce7f969f528be9
Jesse Lappalainen <jesse.m.lappalainen@gmail.com>
2026-06-20 06:34:25 +0300

    Feat: Add spawn animation

--- game/Scripts/bonus_item.gd
+++ game/Scripts/bonus_item.gd
@@ -2,6 +2,12 @@ extends Area2D
 
 signal collision
 
+func _ready() -> void:
+	$Sprite2D.scale = Vector2.ONE
+	var tween := create_tween()
+	tween.tween_property($Sprite2D, "scale", Vector2(2.0, 2.0), 0.2).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN)
+	tween.tween_property($Sprite2D, "scale", Vector2.ONE, 0.2).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+
 func _on_body_entered(body: Node2D) -> void:
 	collision.emit()
 	queue_free()
